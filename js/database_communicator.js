1aee4b7fb09cd535dd454d34caed412184e49bc2
Yash <yashsood978@gmail.com>
2022-03-11 00:57:26 +0800

    fixes

---
+++ js/database_communicator.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB8UoZ9havrz_3MJYxNRQrKwvxjEHAOY1g",
+    authDomain: "notify-yash.firebaseapp.com",
+    projectId: "notify-yash",
+    storageBucket: "notify-yash.appspot.com",
+    messagingSenderId: "633182970585",
+    appId: "1:633182970585:web:5ebd21aaace643760c54c8",
+    measurementId: "G-951R5EQWCZ"
+  };
+
+const app = initializeApp(firebaseConfig);
