44ded9f1794e1ee8015880bd536f31593801b44b
Yash <yashsood978@gmail.com>
2022-03-13 19:50:07 +0800

    database implementation

--- js/database_communicator.js
+++ js/database_communicator.js
@@ -1,13 +1,78 @@
-import { initializeApp } from 'firebase/app';
+const firebase = require("firebase");
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+app.use(cors());
 
-const firebaseConfig = {
-    apiKey: "AIzaSyB8UoZ9havrz_3MJYxNRQrKwvxjEHAOY1g",
-    authDomain: "notify-yash.firebaseapp.com",
-    projectId: "notify-yash",
-    storageBucket: "notify-yash.appspot.com",
-    messagingSenderId: "633182970585",
-    appId: "1:633182970585:web:5ebd21aaace643760c54c8",
-    measurementId: "G-951R5EQWCZ"
-  };
+// Required for side-effects
+require("firebase/firestore");
 
-const app = initializeApp(firebaseConfig);
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+firebase.initializeApp({
+  apiKey: "AIzaSyB8UoZ9havrz_3MJYxNRQrKwvxjEHAOY1g",
+  authDomain: "notify-yash.firebaseapp.com",
+  projectId: "notify-yash",
+});
+var db = firebase.firestore();
+
+app.listen(port, () => {
+  console.log("============================================");
+  console.log(`Server Running & listening on port ${port}`)
+  console.log("============================================");
+});
+
+app.get('/addNote/:receivedData', (req, response) => {
+
+    var receivedData = req.params["receivedData"];
+
+    console.log("[UPLOAD] Uploading '" + receivedData + "' to the database now" )
+    
+
+    var currentdate = new Date(); 
+    var datetime = currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + "@"  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+
+    db.collection("notes").add({
+      note_text: receivedData,
+      dateTime: datetime,
+    })
+    .then((docRef) => {
+      response.send(("Document for '" + receivedData +"' written with ID: ", docRef.id))
+    })
+    .catch((error) => {
+      console.error("Error adding document: ", error);
+    });
+});
+
+
+app.get('/getNotes', (req, response) => {
+  var dataToSendNow = [];
+  db.collection("notes").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      var temp = {
+        'data': null
+      }
+      temp.data = doc.data();
+      temp.data["id"] = doc.id;
+      dataToSendNow.push(temp)
+    });
+    console.log("[DOWNLOAD] Retrieved " + String(dataToSendNow.length) + " notes from database")
+    response.send(dataToSendNow)
+});
+});
+
+app.get('/deleteNote/:noteId'), (req, response) => {
+  var receivedData = req.params["noteId"];
+  db.collection("notes").doc(receivedData).delete().then(() => {
+      console.log("Document successfully deleted!");
+      response.send("Successfully Deleted")
+  }).catch((error) => {
+      console.error("Error removing document: ", error);
+      response.send("Error removing document: ", error)
+  });
+}
---
+++ js/main.js
@@ -0,0 +1,70 @@
+function addNote(){
+    dataToSend = document.getElementById('inputTextToSend').value
+    if (dataToSend == ""){
+        document.getElementById('emptyFieldAlertDivider').style.display = '';
+        
+        setInterval(function(){ 
+            document.getElementById('emptyFieldAlertDivider').style.display = 'none'; 
+        }, 1500);
+
+    }else{
+        document.getElementById('loadingDivider').style.display = '';
+        fetch("http://localhost:3000/addNote/" + dataToSend)
+        .then((res) => {
+            res.text().then(function (data) {
+                console.log("Note saved with id: ",data)
+            });
+            document.getElementById('inputTextToSend').value = ""
+            document.getElementById('loadingDivider').style.display = 'none';
+        })
+        .catch((err) => {
+            /* handle errors */
+            console.log("ERROR on fetch request", err); 
+        })
+    }
+}
+
+/* Open the sidenav */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+    loadNotes();
+  }
+  
+/* Close/hide the sidenav */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById('noteListDivider').innerHTML = '';
+}
+
+function loadNotes(){
+    fetch("http://localhost:3000/getNotes")
+    .then((res) => {
+        res.json().then(function (data) {
+            for (let i = 0; i < data.length; i++) {
+                var tempId = data[i]['data']['id']
+                var tempText = data[i]['data']['note_text']
+                var tempTime = data[i]['data']['dateTime']
+                document.getElementById('noteListDivider').innerHTML += 
+                `
+                <div id="noteCardElement" class="card">
+                    <p id="noteTextInCard">${tempText}</p>
+                    <img src="/images/deleteIcon.png" id="deleteIcon" onclick="deleteNote('${tempId}')">
+                </div>
+                `;
+            }
+        })
+    })
+    .catch((err) => {
+
+    })
+}
+
+function deleteNote(noteId){
+    fetch("http://localhost:3000/deleteNote/" + noteId)
+        .then((res) => {
+            console.log(res)
+        })
+        .catch((err) => {
+            console.log("ERROR on fetch request", err); 
+    })
+}
